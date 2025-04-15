fb1834d24e4bd6b2a1bcc0af7f94916e7d418616
Kalawat <amit.kalawat@gmail.com>
2025-04-15 13:04:25 -0400

    Commit

--- lib/lambda_cpu_service.ts
+++ lib/lambda_cpu_service.ts
@@ -1,37 +1,53 @@
-import * as core from "@aws-cdk/core";
-import * as apigateway from "@aws-cdk/aws-apigateway";
-import * as lambda from "@aws-cdk/aws-lambda";
-import * as s3 from "@aws-cdk/aws-s3";
-import * as iam from "@aws-cdk/aws-iam";
+import { Duration, RemovalPolicy } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as ssm from "aws-cdk-lib/aws-ssm";
 
-export class LambdaMain extends core.Construct {
-  constructor(scope: core.Construct, id: string) {
+export class LambdaMain extends Construct {
+  constructor(scope: Construct, id: string) {
     super(scope, id);
 
     const bucket = new s3.Bucket(this, "LambdaStore", {
-      removalPolicy: core.RemovalPolicy.DESTROY,
+      removalPolicy: RemovalPolicy.DESTROY,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      autoDeleteObjects: true
     });
     
-    const duration = core.Duration.seconds(900);
+    const duration = Duration.seconds(300);
     
-    const lambdarole = new iam.Role(this, "lambdaRole", {assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')});
-    lambdarole.addManagedPolicy({managedPolicyArn: 'arn:aws:iam::aws:policy/CloudWatchLambdaInsightsExecutionRolePolicy'});
-    lambdarole.addManagedPolicy({managedPolicyArn: 'arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole'});
+    const lambdarole = new iam.Role(this, "lambdaRole", {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')
+    });
+    lambdarole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('CloudWatchLambdaInsightsExecutionRolePolicy')
+    );
+    lambdarole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole')
+    );
     
-    const layerArn = "arn:aws:lambda:"+ process.env.CDK_DEFAULT_REGION +":580247275435:layer:LambdaInsightsExtension:2";
+    // Use SSM parameter for latest Lambda Insights layer
+    const layerArn = ssm.StringParameter.valueForStringParameter(
+      this,
+      '/aws/service/aws-lambda-insights/extension/default'
+    );
     const layer = lambda.LayerVersion.fromLayerVersionArn(this, `LayerFromArn`, layerArn);
 
     const handler = new lambda.Function(this, "LambdaCPU", {
-      runtime: lambda.Runtime.NODEJS_12_X,
-      code: lambda.Code.asset("resources"),
+      runtime: lambda.Runtime.NODEJS_20_X,
+      code: lambda.Code.fromAsset("resources"),
       handler: "lambda_cpu.handler",
       layers: [layer],
       role: lambdarole,
       tracing: lambda.Tracing.ACTIVE,
-      memorySize: 128,
+      memorySize: 256,
       timeout: duration,
       environment: {
-        BUCKET: bucket.bucketName
+        BUCKET: bucket.bucketName,
+        POWERTOOLS_SERVICE_NAME: 'LambdaCPU',
+        POWERTOOLS_METRICS_NAMESPACE: 'LambdaInsightsDemo'
       }
     });
 
@@ -39,7 +55,10 @@ export class LambdaMain extends core.Construct {
 
     const api = new apigateway.RestApi(this, "helloworld-api-cpu", {
       restApiName: "Default API - CPU ",
-      description: "Default API - Test Service - CPU"
+      description: "Default API - Test Service - CPU",
+      deployOptions: {
+        tracingEnabled: true
+      }
     });
 
     const getLambdaIntegration = new apigateway.LambdaIntegration(handler);
